33adbff846e74cfd9493505f7d8261413ba1df80
yhsu1126 <yhsu1126@gmail.com>
2014-12-20 16:14:43 +0800

    add minor change

--- download1.py
+++ download1.py
@@ -1,19 +1,24 @@
+# -*- coding: utf-8 -*-
 import sys
 import os
 import json
 import io
-import http.client;
+import httplib
+import re
 from pyquery import PyQuery as pq
 from lxml import etree
 import urllib
 import networkx as nx
-import queue
+import Queue
 
+# Usage : python download1.py {CHANNEL_LIMIT}
+
+# need to run with python 2.7
 
 def query_youtube_subscriptions(channel_id):
 	url= "/youtube/v3/subscriptions?part=snippet&channelId="+channel_id+"&key=AIzaSyCuYeQya-JMYpWHhK8kCQKxzlrQXam-b0k&maxResults=50"
 
-	connection = http.client.HTTPSConnection("www.googleapis.com")
+	connection = httplib.HTTPSConnection("www.googleapis.com")
 	connection.request("GET", url)
 	response = connection.getresponse()
 	data = response.read().decode("utf-8")
@@ -23,7 +28,7 @@ def query_youtube_subscriptions(channel_id):
 def query_youtube_playlists(channel_id):
 	url= "/youtube/v3/playlists?part=snippet&channelId="+channel_id+"&key=AIzaSyCuYeQya-JMYpWHhK8kCQKxzlrQXam-b0k&maxResults=50"
 
-	connection = http.client.HTTPSConnection("www.googleapis.com")
+	connection = httplib.HTTPSConnection("www.googleapis.com")
 	connection.request("GET", url)
 	response = connection.getresponse()
 	data = response.read().decode("utf-8")
@@ -33,7 +38,7 @@ def query_youtube_playlists(channel_id):
 def query_youtube_playlistitem(playlist_id):
 	url= "/youtube/v3/playlistItems?part=snippet&playlistId="+playlist_id+"&key=AIzaSyCuYeQya-JMYpWHhK8kCQKxzlrQXam-b0k&maxResults=50"
 
-	connection = http.client.HTTPSConnection("www.googleapis.com")
+	connection = httplib.HTTPSConnection("www.googleapis.com")
 	connection.request("GET", url)
 	response = connection.getresponse()
 	data = response.read().decode("utf-8")
@@ -43,13 +48,15 @@ def query_youtube_playlistitem(playlist_id):
 def query_youtube_page_for_related_videos(v):
 	url= "/watch?v="+v
 
-	connection = http.client.HTTPSConnection("www.youtube.com")
+	connection = httplib.HTTPSConnection("www.youtube.com")
 	connection.request("GET", url)
 	response = connection.getresponse()
 	data = response.read().decode("utf-8")
 	d = pq(data)
 	related_video = list()
 	for li in d("#watch-related li"):
+		# a 的格式要小心
+		# related video 有可能有
 		a = pq(li).find('a').attr('href')
 		if a != None:
 			related_video.append(a)
@@ -57,24 +64,23 @@ def query_youtube_page_for_related_videos(v):
 
 def query_youtube_video(v):
 	url= "/youtube/v3/videos?part=snippet&id="+v+"&key=AIzaSyCuYeQya-JMYpWHhK8kCQKxzlrQXam-b0k&maxResults=50"
-
-	connection = http.client.HTTPSConnection("www.googleapis.com")
+	connection = httplib.HTTPSConnection("www.googleapis.com")
 	connection.request("GET", url)
 	response = connection.getresponse()
 	data = response.read().decode("utf-8")
 
 	return json.loads(data)
-
+	
+def query_submission_web(channel_id):
 	
 
-if __name__ == "__main__":	
-	
-	start_channel_id = sys.argv[1]
-	channel_limit = int(sys.argv[2])
+if __name__ == "__main__":
+	start_channel_id = 'UC26zQlW7dTNcyp9zKHVmv4Q'
+	channel_limit = int(sys.argv[1])
 	graph = nx.DiGraph()
 
 	
-	download_queue = queue.Queue()
+	download_queue = Queue.Queue()
 	download_queue.put(start_channel_id)
 
 	while channel_limit > 0 and not download_queue.empty():
@@ -85,7 +91,6 @@ if __name__ == "__main__":
 		graph.add_node(channel_id, type='channel')
 
 		playlists = query_youtube_playlists(channel_id)
-
 		for playlist in playlists['items']:
 			playlist_item = query_youtube_playlistitem(playlist['id'])
 			if playlist_item['pageInfo']['totalResults'] > 50:
@@ -93,16 +98,20 @@ if __name__ == "__main__":
 
 			for item in playlist_item['items']:
 				v = item['snippet']['resourceId']['videoId']
-				print(v)
-				break
 				if not graph.has_node(v):
 					graph.add_node(v, type='video', channelId=item['snippet']['channelId'],playlist=playlist['id'], title=item['snippet']['title'], description=item['snippet']['description'], publishedAt=item['snippet']['publishedAt'])
 				graph.add_edge(channel_id, v, type='has')
-
-				# for related_video in query_youtube_page_for_related_videos(v):
-				# 	if not graph.has_node(related_video):
-				# 		video_info = query_youtube_video(related_video)
-				# 		graph.add_node(v, type='video', channelId=video_info['snippet']['channelId'], title=video_info['snippet']['title'], description=video_info['snippet']['description'], publishedAt=video_info['snippet']['publishedAt'])
+				
+				# related video part
+				
+				for related_video in query_youtube_page_for_related_videos(v):
+					if not graph.has_node(related_video):
+						p=re.compile('.*?watch\?v=(.*?)($|&)')
+						s=p.search(related_video)
+						related_video=s.group(1)
+						video_info = query_youtube_video(related_video)
+						v1=video_info['items'][0]['id']
+				 		graph.add_node(v, type='video', channelId=video_info['items'][0]['snippet']['channelId'], title=video_info['items'][0]['snippet']['title'], description=video_info['items'][0]['snippet']['description'], publishedAt=video_info['items'][0]['snippet']['publishedAt'])
 
 
 
@@ -112,12 +121,3 @@ if __name__ == "__main__":
 				download_queue.put(item['snippet']['resourceId']['channelId'])
 
 		channel_limit -= 1
-
-
-		
-		
-
-
-
-
-	# query_youtube_page('0DRW62VmxYU')
