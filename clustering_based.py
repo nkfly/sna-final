8f0a51800f0400065a50a11cab61563acb925612
nkfly <nkfly@github.com>
2015-01-03 19:38:24 +0800

    clustering with initialized cluster number

--- clustering_based.py
+++ clustering_based.py
@@ -10,12 +10,15 @@ import urllib
 import networkx as nx
 import datetime
 import codecs
+import math
 
 import numpy as np
 from numpy.testing import assert_equal, assert_array_equal
 
 from sklearn.cluster.affinity_propagation_ import AffinityPropagation, \
 									affinity_propagation
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
 from sklearn.datasets.samples_generator import make_blobs
 from sklearn.metrics import euclidean_distances
 
@@ -93,6 +96,68 @@ def count_accuracy(mygraph, predict_candidates, labels):
 				correct += 1
 	return correct/(len_predict_candidates*(len_predict_candidates-1)/2)
 
+def isEnglish(s):
+    try:
+        s.encode('ascii')
+    except UnicodeEncodeError:
+        return False
+    else:
+        return True
+
+
+def make_bigram_list(sentence):
+	if isEnglish(sentence):
+		return sentence.split()
+	bigram_list = list()
+	for i in range(len(sentence)-1):
+		bigram_list.append(sentence[i:i+2])
+	return bigram_list
+
+
+def convert_to_tfidf_bigram(mygraph, predict_candidates):
+	dimension_dic = dict()
+	df_dic = dict()
+	tfidf_vectors = []
+	for node in predict_candidates:
+		bigram_list = make_bigram_list(mygraph.node[node]['title'])
+		bigram_set_for_df = set()
+		vector = dict()
+		for bigram in bigram_list:
+			if bigram not in dimension_dic:
+				dimension_dic[bigram] = len(dimension_dic)
+			if dimension_dic[bigram] not in vector:
+				vector[dimension_dic[bigram]] = 1
+			else:
+				vector[dimension_dic[bigram]] = vector[dimension_dic[bigram]] + 1
+			bigram_set_for_df.add(bigram)
+		for bigram in bigram_set_for_df:
+			if dimension_dic[bigram] not in df_dic:
+				df_dic[dimension_dic[bigram]] = 1
+			else:
+				df_dic[dimension_dic[bigram]] = df_dic[dimension_dic[bigram]] + 1
+		tfidf_vectors.append(vector)
+
+
+	number_of_documents = len(predict_candidates)
+
+	for vector in tfidf_vectors:
+		for dimension in vector:
+			vector[dimension] = math.log(1+vector[dimension])*math.log(1+number_of_documents/df_dic[dimension])
+	dimension_max = len(dimension_dic)
+	return tfidf_vectors, dimension_max
+
+
+def to_compact_vectors(tfidf_vectors, dimension_max):
+	compact_vectors = []
+	for vector in tfidf_vectors:
+		v = []
+		for i in range(dimension_max):
+			if i not in vector:
+				v.append(0)
+			else:
+				v.append(vector[i])
+		compact_vectors.append(v)
+	return compact_vectors
 
 
 if __name__ == '__main__':
@@ -108,12 +173,26 @@ if __name__ == '__main__':
 
 		if len(predict_candidates) == 0:
 			continue
-		affinity_matrix = construct_affinity_matrix(mygraph,  predict_candidates)
-		# print(affinity_matrix)
 
-		# preference = np.median(affinity_matrix) * 10				
-		# Compute Affinity Propagation
-		cluster_centers_indices, labels = affinity_propagation(affinity_matrix)
+		peek_data_for_playlist_number = set()
+		for node in predict_candidates:			
+			peek_data_for_playlist_number.update(mygraph.node[node]['playlist'])
+		# print(len(peek_data_for_playlist_number))
+		n_clusters = len(peek_data_for_playlist_number)
+		tfidf_vectors, dimension_max = convert_to_tfidf_bigram(mygraph, predict_candidates)
+
+		tfidf_vectors = to_compact_vectors(tfidf_vectors, dimension_max)
+
+		pca = PCA(n_components=n_clusters).fit(tfidf_vectors)
+		estimator = KMeans(init=pca.components_, n_clusters=n_clusters, n_init=1)
+		# estimator = KMeans(init='random', n_clusters=n_clusters, n_init=10)
+		# estimator = KMeans(init='k-means++', n_clusters=n_clusters, n_init=10)
+		
+		estimator.fit(tfidf_vectors)
+		labels = estimator.labels_
+
+		# affinity_matrix = construct_affinity_matrix(mygraph,  predict_candidates)
+		# cluster_centers_indices, labels = affinity_propagation(affinity_matrix)
 		
 		print(count_accuracy(mygraph, predict_candidates, labels))
 
---
+++ testtest.py
@@ -0,0 +1,16 @@
+print(__doc__)
+
+from time import time
+import numpy as np
+
+from sklearn import metrics
+from sklearn.cluster import KMeans
+from sklearn.datasets import load_digits
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import scale
+
+np.random.seed(42)
+
+digits = load_digits()
+data = scale(digits.data)
+print(data)
