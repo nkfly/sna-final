76fafbf92d27241dc1230d8283d3ac26384b34c2
nkfly <nkfly@github.com>
2015-01-06 22:17:37 +0800

    predict okk

--- learning_based.py
+++ learning_based.py
@@ -23,6 +23,7 @@ from sklearn.metrics import euclidean_distances
 from sklearn.cluster import MeanShift, estimate_bandwidth
 from sklearn.cluster import DBSCAN
 from dateutil import parser
+from sklearn import svm
 
 
 def read_graph(filename):
@@ -64,9 +65,22 @@ def convert_to_feature_vector(mygraph, node1, node2):
 	df = date_diff(mygraph, node1, node2)
 	return [aff, df]
 
+def count_accuracy(mygraph, predict_candidates, labels):
+	len_predict_candidates = len(predict_candidates)
+	if len_predict_candidates <= 1:
+		return 1
+	correct = 0
+	for i in range(len_predict_candidates):
+		for j in range(i+1, len_predict_candidates):
+			if (mygraph.node[predict_candidates[i]]['playlist'] == mygraph.node[predict_candidates[j]]['playlist']) == (labels[i] == labels[j]):
+				correct += 1
+	return correct/(len_predict_candidates*(len_predict_candidates-1)/2)
+
 
 if __name__ == '__main__':
-	mygraph = read_graph("./firsttry.gpickle")
+	training_graph = "./firsttry.gpickle"
+	test_graph = "./secondtry.gpickle"
+	mygraph = read_graph(training_graph)
 	x = []
 	y = []
 
@@ -91,15 +105,57 @@ if __name__ == '__main__':
 					y.append(-1)					
 				x.append(convert_to_feature_vector(mygraph, predict_candidates[i], predict_candidates[j]))
 	with open('training.txt', 'w') as w:
-		w.write(str(y[i]) + ': ')
+		w.write(str(y[i]) + ' ')
 		for i in range(len(x)):
-			w.write(str(1+i) + ':' + str(x[i]))
-		w.write('\n')		
-	
+			w.write(str(1+i) + ':' + str(x[i])+ ' ')
+		w.write('\n')
+
+	print('before training')
+
+	clf = svm.SVC()
+	clf.fit(x, y)
+
+	print('after fitting')
+
+
+	accuracy_from_svm = 0
+	mygraph = read_graph(test_graph)
+	sample_number = 0
+	for node in mygraph.nodes():
+		if mygraph.node[node]['type'] == 'video':
+			continue
+
+		predict_candidates = list()
+		for neighbor in mygraph.neighbors(node):
+			if mygraph.node[neighbor]['type'] == 'video':
+				predict_candidates.append(neighbor)
+
+		if len(predict_candidates) == 0:
+			continue
+		sample_number += 1
+		labels = [0 for i in range(len(predict_candidates))]
+		group_index = 1
+		for i in range(len(predict_candidates)):
+			if labels[i] != 0:
+				predict_candidates[i] = group_index
+			for j in range(i+1, len(predict_candidates)):
+				ret = clf.predict(convert_to_feature_vector(mygraph, predict_candidates[i], predict_candidates[j]))
+				if ret[0] == 1:
+					predict_candidates[i] = group_index
+			group_index += 1
+		accuracy_from_svm += count_accuracy(mygraph, predict_candidates, labels)
+
+
+		print(' accuracy_from_svm:'+str(accuracy_from_svm/sample_number))
+
+
+
+
+
+
+
+		
 
 
 	
-
-
-
-	print('hello')
+	
